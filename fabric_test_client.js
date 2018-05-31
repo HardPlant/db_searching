bbe51ab7ee8c354fdf663a1d1147f5bdd11bdd7a
HardPlant <abc7988se@naver.com>
2018-05-31 10:29:20 +0900

    added occupication

--- fabric_test_client.js
+++ fabric_test_client.js
@@ -6,16 +6,16 @@ let BusinessNetworkConnection = composer.BusinessNetworkConnection;
 
 let connection = new BusinessNetworkConnection();
 let definition = "";
-
+let factory = definition.getFactory();
 main();
 
-async function main(){
+async function main() {
     definition = await connection.connect(cardName);
 
     var before = moment().unix()
     await removeAll('org.hackerton.Hospital');
     var after = moment().unix()
-    console.log(after-before)
+    console.log(after - before)
 
     var before = moment().unix()
     await addHospital("A병원");
@@ -24,25 +24,26 @@ async function main(){
     await addHospital("D병원");
     await addHospital("E병원");
     var after = moment().unix()
-    console.log(after-before)
+    console.log(after - before)
 
     var before = moment().unix()
     let HospitalList = await getAll('org.hackerton.Hospital');
-    for(let i=0; i < HospitalList.length; i++){
+    for (let i = 0; i < HospitalList.length; i++) {
         console.log(`${HospitalList[i].HospitalID}: ${HospitalList[i].name}`);
     }
     var after = moment().unix()
-    console.log(after-before)
+    console.log(after - before)
 }
 
-function login(){}
-function session(){}
+function login() {}
 
-async function request(requester, x, y){
+function session() {}
+
+async function request(requester, x, y) {
     let serializer = definition.getSerializer();
 
     let registry = connection.getAssetRegistry('org.hackerton.Patient');
-    let factory = definition.getFactory();
+
     let currentTime = moment().unix();
 
     patient = factory.newResource('org.hackerton', 'Patient', currentTime)
@@ -53,49 +54,47 @@ async function request(requester, x, y){
     await this.registry.addAll([request]);
 
     let request = serializer.fromJSON({
-        "$class" : 'org.hackerton.requestEmergencyCar',
-        'patient' : patient,
-        'requester' : requester,
-        'x' : x,
-        'y' : y
+        "$class": 'org.hackerton.requestEmergencyCar',
+        'patient': patient,
+        'requester': requester,
+        'x': x,
+        'y': y
     })
 
     await connection.submitTransaction(request)
 }
 
-async function addHospital(name){
+async function addHospital(name) {
     let registry = await connection.getParticipantRegistry('org.hackerton.Hospital');
-    let factory = definition.getFactory();
+
 
     let currentTime = moment().unix();
-    hospital = factory.newResource('org.hackerton','Hospital', currentTime.toString());
+    hospital = factory.newResource('org.hackerton', 'Hospital', currentTime.toString());
     hospital.name = name;
     await registry.add(hospital);;
 }
 
-async function getAll(FDQN){
+async function getAll(FDQN) {
     let registry = await connection.getParticipantRegistry(FDQN);
     let list = await registry.getAll();
-    
+
     return list;
 }
-async function removeAll(FDQN){
+async function removeAll(FDQN) {
     let registry = await connection.getParticipantRegistry(FDQN);
     let list = await registry.getAll();
     await registry.removeAll(list)
 }
-function removeAllHospital(){
+
+function addDoctor() {
 
 }
 
-function addDoctor(){
+function addSchedule() {}
 
-}
-function getSchedule(){}
-function setSchedule(){}
-function deleteSchedule(){}
+function deleteSchedule() {}
 
 
-function addCar(){}
-function modifyCar(){}
+function addCar() {}
 
+function modifyCar() {}
